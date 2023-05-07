6e3da56b0598ed9ce12b07b7855c82e10cc87999
Federico Knüssel <fknussel@gmail.com>
2023-05-07 14:47:31 +1000

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:styled-components-a11y/strict",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "styled-components-a11y"],
+  rules: {},
+};
---
+++ src/app.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { styled } from "@compiled/react";
+
+const Button = styled.span`
+  border-radius: 3px;
+  background: palevioletred;
+  color: white;
+  padding: 20px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+  max-width: 200px;
+  font-family: sans-serif;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  max-width: 300px;
+  display: block;
+`;
+
+export function App() {
+  return (
+    <Container>
+      <Button onClick={() => {}}>Normal Button</Button>
+      <Image src="https://github.com/fed.png" />
+    </Container>
+  );
+}
